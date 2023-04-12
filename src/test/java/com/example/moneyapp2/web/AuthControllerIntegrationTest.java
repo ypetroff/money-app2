a62dae1d8a954b4e83dcf864a150f9404532abed
Yordan Petrov <yordan.petroff@gmail.com>
2023-04-12 19:12:47 +0300

    data.sql for tests

--- src/test/java/com/example/moneyapp2/web/AuthControllerIntegrationTest.java
+++ src/test/java/com/example/moneyapp2/web/AuthControllerIntegrationTest.java
@@ -3,24 +3,19 @@ package com.example.moneyapp2.web;
 import com.example.moneyapp2.model.dto.user.UserRegisterDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
-@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase
 @SpringBootTest
 @AutoConfigureMockMvc
 class AuthControllerIntegrationTest {
