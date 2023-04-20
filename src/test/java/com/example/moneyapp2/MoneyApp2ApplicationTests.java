629198f0f61f1a063b834f3a45b04a55c4be0068
Yordan Petrov <yordan.petroff@gmail.com>
2023-04-20 20:15:21 +0300

    fix jdbc bug for mysql

--- src/test/java/com/example/moneyapp2/MoneyApp2ApplicationTests.java
+++ src/test/java/com/example/moneyapp2/MoneyApp2ApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.moneyapp2;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class MoneyApp2ApplicationTests {
 
 	@Test
--- src/test/java/com/example/moneyapp2/web/AuthControllerIntegrationTest.java
+++ src/test/java/com/example/moneyapp2/web/AuthControllerIntegrationTest.java
@@ -1,17 +1,21 @@
 package com.example.moneyapp2.web;
 
 import com.example.moneyapp2.model.dto.user.UserRegisterDTO;
+import com.example.moneyapp2.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -26,7 +30,7 @@ class AuthControllerIntegrationTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void registerUser() throws Exception {
+    void registerUserWithCorrectInput() throws Exception {
 
         UserRegisterDTO user = UserRegisterDTO.builder()
                 .username("test")
@@ -41,6 +45,33 @@ class AuthControllerIntegrationTest {
                 .content(objectMapper.writeValueAsString(user))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    void registerUserWithIncorrectInput() throws Exception {
+
+        UserRegisterDTO user = UserRegisterDTO.builder()
+                .username("t")
+                .email("test_test.bg")
+                .firstName("Te")
+                .lastName("")
+                .password("123")
+                .confirmPassword("345")
+                .build();
+
+        mockMvc.perform(post("/api/auth/register")
+                        .content(objectMapper.writeValueAsString(user))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+             //todo   .andExpect(ResponseEntity.)
+//        {
+//            "lastName": "Last name should be between 3 and 15 characters long",
+//                "firstName": "First name should be between 3 and 15 characters long",
+//                "password": "Password should be at least 3 characters long",
+//                "confirmPassword": "Passwords don't match",
+//                "email": "Enter valid email address",
+//                "username": "Username should be between 3 and 23 characters long"
+//        }
     }
 
     @Test
