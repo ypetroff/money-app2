6f8eda32f26ec999eb39ec763ef5b47b27fc3730
Yordan Petrov <yordan.petroff@gmail.com>
2023-05-15 14:58:25 +0300

    tests for IncomeCategoryServiceTest

--- src/test/java/com/example/moneyapp2/service/IncomeCategoryServiceTest.java
+++ src/test/java/com/example/moneyapp2/service/IncomeCategoryServiceTest.java
@@ -11,10 +11,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -64,6 +66,49 @@ class IncomeCategoryServiceTest {
     }
 
     @Test
-    void categoriesToString() {
+    void categoriesToStringWithOneCategory() {
+
+        IncomeCategoryEntity category = IncomeCategoryEntity.builder()
+                .category(IncomeCategory.CASH)
+                .build();
+
+        when(this.mockIncomeCategoryRepository.findAll())
+                .thenReturn(List.of(category));
+
+        List<String> categories = toTest.categoriesToString();
+
+        assertEquals(1, categories.size());
+        String actualCategoryName = categories.get(0);
+        assertEquals(category.getCategory().name(), actualCategoryName);
+    }
+
+    @Test
+    void categoriesToStringWithTwoCategory() {
+
+        IncomeCategoryEntity category1 = IncomeCategoryEntity.builder()
+                .category(IncomeCategory.CASH)
+                .build();
+        IncomeCategoryEntity category2 = IncomeCategoryEntity.builder()
+                .category(IncomeCategory.BANK)
+                .build();
+
+        when(this.mockIncomeCategoryRepository.findAll())
+                .thenReturn(List.of(category1, category2));
+
+        List<String> categories = toTest.categoriesToString();
+
+        assertEquals(2, categories.size());
+        String actualCategoryName1 = categories.get(0);
+        String actualCategoryName2 = categories.get(1);
+        assertEquals(category1.getCategory().name(), actualCategoryName1);
+        assertEquals(category2.getCategory().name(), actualCategoryName2);
+    }
+
+    @Test
+    void categoriesToStringWithEmptyRepository() {
+
+        List<String> categories = toTest.categoriesToString();
+
+        assertEquals(0, categories.size());
     }
 }
